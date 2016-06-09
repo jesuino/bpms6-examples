228d3b1b6fb99bacf7d1958bc8aac5c52154de2c
William Antonio <william.fatecsjc@gmail.com>
2016-06-09 16:45:19 -0300

    Adding example for BxMS 6.2(must update persistence.xml)

---
+++ bpms62/bpms62-node-instance-query/README.md
@@ -0,0 +1,3 @@
+# Sample standalone BPM Suite 6.3 application to query nodes
+
+The simplest standalone BPM Suite 6.3 application with persistence and some sample queries
---
+++ bpms62/bpms62-node-instance-query/src/main/java/com/redhat/gss/bpms/data/HelloWorldProcessRunner.java
@@ -0,0 +1,24 @@
+package com.redhat.gss.bpms.data;
+
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.manager.RuntimeEngine;
+import org.kie.api.runtime.manager.RuntimeManager;
+import org.kie.internal.runtime.manager.context.EmptyContext;
+
+import com.redhat.gss.bpms.Util;
+
+public class HelloWorldProcessRunner {
+
+	public static void main(String[] args) throws Exception {
+        RuntimeManager manager = Util.getRuntimeManager();
+        RuntimeEngine engine = manager.getRuntimeEngine(EmptyContext.get());
+        KieSession kSession = engine.getKieSession();
+		for (int i = 0; i < 50000; i++) {
+			System.out.println("Starting process number " + i);
+			kSession.startProcess("com.redhat.gss.bpms.helloProcess");
+		}
+		manager.disposeRuntimeEngine(engine);
+		manager.close();
+	}
+	
+}
