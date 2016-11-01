853b22523152763232e5d1367d987717a02116db
William Antonio <william.fatecsjc@gmail.com>
2016-11-01 11:14:17 -0200

    removing outdated files

--- bpms63/decision-server-remote-jmsclient-on-eap/src/main/java/org/fxapps/bpms/decisionserver/JaxRsApp.java
+++
@@ -1,9 +0,0 @@
-package org.fxapps.bpms.decisionserver;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("rest")
-public class JaxRsApp extends Application {
-
-}
