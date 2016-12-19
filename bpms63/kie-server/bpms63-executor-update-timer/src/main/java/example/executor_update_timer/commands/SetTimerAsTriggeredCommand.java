dd1bd1016b175f22666960f1c1415a1b1c2c5de0
William Antonio <william.fatecsjc@gmail.com>
2016-12-19 14:52:54 -0200

    using transaction manager

--- bpms63/kie-server/bpms63-executor-update-timer/src/main/java/example/executor_update_timer/commands/SetTimerAsTriggeredCommand.java
+++ bpms63/kie-server/bpms63-executor-update-timer/src/main/java/example/executor_update_timer/commands/SetTimerAsTriggeredCommand.java
@@ -1,5 +1,8 @@
 package example.executor_update_timer.commands;
 
+import javax.naming.InitialContext;
+import javax.transaction.TransactionManager;
+
 import org.kie.api.executor.Command;
 import org.kie.api.executor.CommandContext;
 import org.kie.api.executor.ExecutionResults;
@@ -17,6 +20,8 @@ import example.executor_update_timer.service.UpdateTimerService;
 public class SetTimerAsTriggeredCommand implements Command {
 
 	public ExecutionResults execute(CommandContext ctx) throws Exception {
+		TransactionManager tm = (TransactionManager) InitialContext.doLookup("java:jboss/TransactionManager");
+		tm.begin();
 		UpdateTimerService updateTimerService = UpdateTimerService.Factory
 				.get();
 		String identifier = (String) ctx.getData().get("identifier");
@@ -24,6 +29,7 @@ public class SetTimerAsTriggeredCommand implements Command {
 		String piidStr = String.valueOf(ctx.getData().get("processInstanceId"));
 		long piid = Long.parseLong(piidStr);
 		updateTimerService.setAsTriggered(identifier, piid);
+		tm.commit();
 		return null;
 	}
 
